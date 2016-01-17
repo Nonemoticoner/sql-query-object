b899028c1420fb929a039c8fcf8a89db31c538dd
Nonemoticoner <nonemoticoner@gmail.com>
2016-01-17 01:29:14 +0100

    Setup

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * sql-query-object
+ * https://github.com/Nonemoticoner/sql-query-object
+ *
+ * Copyright (c) 2016 Nonemoticoner
+ * Licensed under the MIT license.
+ */
+
+module.exports = {
+	// to be written
+};
